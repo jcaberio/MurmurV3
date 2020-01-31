d646b569e4b76bedb33b3072a4010270943a5f17
Eugene Pankov <john.pankov@gmail.com>
2020-01-31 14:21:41 +0300

    more cleanup

--- setup.py
+++ setup.py
@@ -16,8 +16,8 @@ A few quick NOTES and WARNINGS:
 The implementation of MurMur that is used in this code makes the
 following assumptions about your machine:
 
-	1. A 4-byte value can be read from any address without crashing
-	2. sizeof(int) == 4
+    1. A 4-byte value can be read from any address without crashing
+    2. sizeof(int) == 4
 
 It will also not produce the same results on little-endian and big-endian
 machines.
@@ -26,9 +26,6 @@ I believe it would be easily possible to get around these limitations.
 
 .. _MurmurHash 2.0: http://murmurhash.googlepages.com/
 """
-import ez_setup
-ez_setup.use_setuptools()
-
 from setuptools import setup, Extension
 
 setup(
@@ -37,21 +34,21 @@ setup(
     license='MIT',
     author='Jorick Caberio, Bryan McLemore',
     description='MurmurV3 for Python 3.7',
-	url = 'https://github.com/jcaberio/MurmurV3',
-	download_url = 'https://github.com/jcaberio/MurmurV3/tarball/0.5',
-    zip_safe=False, # I'm not sure if it is egg safe so I'm erring on the side of caution.
+    url='https://github.com/jcaberio/MurmurV3',
+    download_url='https://github.com/jcaberio/MurmurV3/tarball/0.5',
+    zip_safe=False,  # I'm not sure if it is egg safe so I'm erring on the side of caution.
     long_description=__doc__,
-	ext_modules = [
-		Extension(
-			'murmur',
-			["murmur/murmur.cpp"],
-			language='c++'
-		)
-	],
+    ext_modules=[
+        Extension(
+            'murmur',
+            ["murmur/murmur.cpp"],
+            language='c++'
+        )
+    ],
     platforms='any',
     classifiers=[
-	    'Programming Language :: Python',
-	    'Programming Language :: Python :: 3',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
@@ -60,5 +57,5 @@ setup(
         'Topic :: Software Development',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
-	packages=[]
+    packages=[]
 )
